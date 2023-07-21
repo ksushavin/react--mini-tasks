6f1de10b83cf77783a16b93cbb5e5418866c5553
Oksana Bulakh <ksushavin15@gmail.com>
2023-07-21 20:39:48 +0200

    розмітка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { Recipe } from './Recipe/Recipe';
+import recipes from '../recipes.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {recipes.map((recipe) => (
+        <Recipe key={recipe.id} recipe={recipe} />
+      ))}
+    </>
   );
 };
