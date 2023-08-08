4dd40045bca1284993eaa8fb39bcb4b7809a93c7
Oksana Bulakh <ksushavin15@gmail.com>
2023-08-08 17:09:28 +0200

    change Sticker as component

--- src/components/Sticker/Sticker.js
+++ src/components/Sticker/Sticker.js
@@ -1,40 +1,27 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from 'components/Box';
 
-export class Sticker extends Component {
-  // state = {
-  //   isOpen: false,
-  // };
+export const Sticker = props => {
+  const {
+    sticker: { img, label },
+    idx,
+    onSelect,
+    isSelected,
+    onToggle,
+  } = props;
 
-  // toggleLable = () => {
-  //   this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-  // };
-
-  render() {
-    const {
-      sticker: { img, label },
-      idx,
-      onSelect,
-      isSelected,
-      onToggle,
-    } = this.props;
-
-    return (
-      <Box
-        onClick={() => {
-          onSelect(label);
-          onToggle(idx);
-          // this.toggleLable();
-        }}
-      >
-        <img src={img} alt={label} width={120} />
-        {/* {this.state.isOpen && <p>{label}</p>} */}
-        {isSelected && <p>{label}</p>}
-      </Box>
-    );
-  }
-}
+  return (
+    <Box
+      onClick={() => {
+        onSelect(label);
+        onToggle(idx);
+      }}
+    >
+      <img src={img} alt={label} width={120} />
+      {isSelected && <p>{label}</p>}
+    </Box>
+  );
+};
 
 Sticker.propTypes = {
   sticker: PropTypes.shape({
@@ -46,3 +33,60 @@ Sticker.propTypes = {
   isSelected: PropTypes.bool.isRequired,
   onToggle: PropTypes.func.isRequired,
 };
+
+//Sticker через клас
+
+// export class Sticker extends Component {
+//   render() {
+//     const {
+//       sticker: { img, label },
+//       idx,
+//       onSelect,
+//       isSelected,
+//       onToggle,
+//     } = this.props;
+
+//     return (
+//       <Box
+//         onClick={() => {
+//           onSelect(label);
+//           onToggle(idx);
+//         }}
+//       >
+//         <img src={img} alt={label} width={120} />
+//         {isSelected && <p>{label}</p>}
+//       </Box>
+//     );
+//   }
+// }
+
+// щоб клік по картці розгортав її. тут потрібен внутрішній стейт
+
+// export class Sticker extends Component {
+//   state = {
+//     isOpen: false,
+//   };
+
+//   toggleLable = () => {
+//     this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+//   };
+
+//   render() {
+//     const {
+//       sticker: { img, label },
+//       onSelect
+//     } = this.props;
+
+//     return (
+//       <Box
+//         onClick={() => {
+//           onSelect(label);
+//           this.toggleLable();
+//         }}
+//       >
+//         <img src={img} alt={label} width={120} />
+//         {this.state.isOpen && <p>{label}</p>}
+//       </Box>
+//     );
+//   }
+// }
